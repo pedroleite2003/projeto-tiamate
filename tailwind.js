26915e775f16801836cbbf8857ace7be0c071142
unknown <pedropinho0680@digitalcollege.internal>
2025-01-10 11:53:41 -0300

    inicio

---
+++ tailwind.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                marrom_black_01: '#292625',
+                marrom_black_02: '#3B2416',
+                marrom_medium_01: '#A97744',
+                marrom_medium_02: '#64221D',
+                marrom_white_01: '#F5E7DA',
+                marrom_white_00: '#E7E1DF',
+                marrom_white_02: '#C7794A',
+                marrom_white_03: '#CF9A5A',
+                bright_color: '#FDF5E3',
+            }
+        },
+    },
+    plugins: [],
+}
